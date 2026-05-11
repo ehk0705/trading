443c9af3beee7b96df4e181d818ee7b642a4883b
ehk0705 <ton-email@example.com>
2026-05-11 07:43:50 -0400

    Mise ├á jour

--- maj.bat
+++ maj.bat
@@ -1,8 +1,13 @@
 SET PATH=%PATH%;C:\Program Files\Git\cmd
+
 cd C:\xampp\htdocs\trading
-git status
-git add .
-git commit -m "Enhancements %date% %time%"
-git push
+
 git status
 
+git add .
+
+git commit -m "Mise à jour "
+
+git push origin main
+
+git status
