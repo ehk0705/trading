d3cedcf288d108803224652e3b4790647224118f
ehk0705 <ton-email@example.com>
2026-05-10 22:37:42 -0400

    Enhancements 2026-05-10 22:37:42,73

--- maj_index.bat
+++ maj_index.bat
@@ -1,9 +1,6 @@
-
-cd C:\xampp\htdocs\trading
 git rm index.html
 git commit -m "Supprimer ancien index.html"
 git push origin main
-
 
 git add index.html
 git commit -m "Ajouter nouveau index.html"
